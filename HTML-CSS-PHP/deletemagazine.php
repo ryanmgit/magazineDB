029dc9b4abe761f3927aaa1f19582163b5a3b7bc
hslevee <38769541+hslevee@users.noreply.github.com>
2018-05-01 16:12:07 -0400

    Update deletemagazine.php

--- HTML-CSS-PHP/deletemagazine.php
+++ HTML-CSS-PHP/deletemagazine.php
@@ -11,7 +11,7 @@ $bid = $row['City'];
 $fid = $row['State'];
 
 if(isset($_POST['yesbutton'])){
-  $query="Delete from magazine where magazineid=:magazineid";
+  $query="DELETE FROM magazine WHERE magazineid=:magazineid";
   $sql=$pdo->prepare($query);
   $sql->execute(
   array(
