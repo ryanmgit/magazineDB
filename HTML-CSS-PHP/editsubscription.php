da772aac1cfc6c3f3c808f8bd163ead9bf88a6b7
ryanmgit <38335749+ryanmgit@users.noreply.github.com>
2018-05-01 20:40:44 -0400

    Update editsubscription.php

--- HTML-CSS-PHP/editsubscription.php
+++ HTML-CSS-PHP/editsubscription.php
@@ -14,7 +14,7 @@ $rid = $row['Magazineid'];
 
 if(isset($_POST['updatebutton'])){
   //execute the following sql statement
-  $query="update subscription set subscriptiondate=:subscriptiondate, subscriptionperiod=:subscriptionperiod, where subscriptionid=:subscriptionid";
+  $query="UPDATE subscription SET subscriptiondate=:subscriptiondate, subscriptionperiod=:subscriptionperiod WHERE subscriptionid=:subscriptionid";
   $sql=$pdo->prepare($query);
   $sql->execute(
   array(":subscriptiondate" => $_POST['subscriptiondate'],
