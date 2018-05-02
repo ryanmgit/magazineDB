9f5fffd3c59eee09d05f35ff89282aeabbe6e4ff
ryanmgit <38335749+ryanmgit@users.noreply.github.com>
2018-05-01 23:14:15 -0400

    Update dbconnection.php

--- HTML-CSS-PHP/dbconnection.php
+++ HTML-CSS-PHP/dbconnection.php
@@ -1,4 +1,4 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;dbname=magazineDB', 'root', 'root');
+$pdo = new PDO('mysql:host=localhost;dbname=magazineDBpreloaded', 'root', 'root');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
