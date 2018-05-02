1ee4f8e7d6ffd3bf9f2d0559aef45dfae91d978b
ryanmgit <38335749+ryanmgit@users.noreply.github.com>
2018-05-01 21:20:49 -0400

    Update editsubscription.php

--- HTML-CSS-PHP/editsubscription.php
+++ HTML-CSS-PHP/editsubscription.php
@@ -7,10 +7,10 @@ $sql = $pdo->prepare($query);
 $sql->execute(array(":subscriptionid" => $_GET['subscriptionid']));
 
 $row = $sql->fetch(PDO::FETCH_ASSOC); //read the next row od the query result into $row
-$aen = $row['Subscriptiondate'];
-$ean = $row['Subscriptionperiod'];
-$kan = $row['Subscriberid'];
-$rid = $row['Magazineid'];
+$subscriptiondate = $row['Subscriptiondate'];
+$subscriptionperiod = $row['Subscriptionperiod'];
+$subscriberid = $row['Subscriberid'];
+$magazineid = $row['Magazineid'];
 
 if(isset($_POST['updatebutton'])){
   //execute the following sql statement
@@ -45,13 +45,17 @@ if(isset($_POST['updatebutton'])){
 <p>
   <form method="post">
 <p>Magazine id:
-  <input type="text" name="magazineid" value="<?= $kan ?>">
+  <input type="text" name="magazineid" value="<?= $magazineis ?>">
 </p>
 <p>Subscription date:
-  <input type="text" name="subscriptiondate" value="<?= $aen ?>">
+  <input type="text" name="subscriptiondate" value="<?= $subscriptiondate ?>">
 </p>
 <p>Subscription period:
-  <input type="text" name="subscriptionperiod" value="<?= $ean ?>">
+	<select>
+		<option value="'Daily'" <?php if ($subscriptionperiod == "Daily") echo "selected='selected'";?> >Daily</option>
+		<option value="'Weekly'" <?php if ($subscriptionperiod == "Weekly") echo "selected='selected'";?> >Weekly</option>
+		<option value="'Monthly'" <?php if ($subscriptionperiod == "Monthly") echo "selected='selected'";?> >Monthly</option>
+	</select>
 </p>
 <p>
   <input type="submit" name="updatebutton" value="update">
