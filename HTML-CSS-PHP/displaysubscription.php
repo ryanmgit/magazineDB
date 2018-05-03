3f68bae3cb69d7582ae14caede252833837857a4
ryanmgit <38335749+ryanmgit@users.noreply.github.com>
2018-05-02 21:34:57 -0400

    Update displaysubscription.php

--- HTML-CSS-PHP/displaysubscription.php
+++ HTML-CSS-PHP/displaysubscription.php
@@ -16,22 +16,24 @@
 
 require_once "dbconnection.php";
 //execute the follpwing quary
-$query="select * from subscription";
+$query="select magazinename, subscribersfname, subscriberslname, subscriptiondate, subscriptionperiod from subscription left join magazine on subscription.magazineid=magazine.magazineid left join subscriber on subscription.subscriberid=subscriber.subscriberid";
 $sql=$pdo->prepare($query);
 $sql->execute();
 
 echo "<table border='1'>";
-echo "<tr><th>Magazine id</th><th>Subscriber id</th><th>Subscriptiondate</th><th>Subscriptionperiod</th><th>Action</th></tr>";
+echo "<tr><th>Magazine Name</th><th>Subscriber fname</th><th>Subscriber lname</th><th>Subscriptiondate</th><th>Subscriptionperiod</th><th>Action</th></tr>";
 //$row = $sql->fetch(PDO::FETCH_ASSOC states that store the next row in the quesry result into $row
   while($row = $sql->fetch(PDO::FETCH_ASSOC)){//print out the query result row by row
     echo "<tr><td>";
-    echo ($row['Magazineid']);
+    echo ($row['magazinename']);
     echo "</td><td>";
-    echo ($row['Subscriberid']);
+    echo ($row['subscribersfname']);
     echo "</td><td>";
-    echo ($row['Subscriptiondate']);
+    echo ($row['subscriberslname']);
     echo "</td><td>";
-    echo ($row['Subscriptionperiod']);
+    echo ($row['subscriptiondate']);
+    echo "</td><td>";
+    echo ($row['subscriptionperiod']);
     echo "</td><td>";
 //clicking on differnt links leads to corresponding editing pages
     echo ('<a href="editsubscription.php?subscriptionid='.$row['Subscriptionid'].'">Edit</a> / ');
